dcabaee38457a7a3e0e22483957462380bd3b0c1
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-10-02 11:16:48 -0700

    Implemented /tracks route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("./models/User");
+require("./models/Track");
 
 const express = require("express");
 const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/authRoutes");
 const requireAuth = require("./middleware/requireAuth");
+const trackRoutes = require("./routes/trackRoutes");
 
 require("dotenv").config();
 
@@ -13,6 +15,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(process.env.MONGO_URI);
 
